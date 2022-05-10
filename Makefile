972146f746b094973e14275e0a8f00a82c5b82c2
bplx <satosummering@protonmail.com>
2022-05-10 19:20:59 -0400

    woohoo, dexah branch

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
 INCLUDE=./include
 SRC=./src
+LIBS=-lcurl
+MINGW_LIBS=-lcurl
 
 make:
-	gcc -I$(INCLUDE) $(SRC)/main.c
+	gcc -I$(INCLUDE) $(LIBS) $(SRC)/main.c
+
+mingwmake:
+	gcc -I$(INCLUDE) $(MINGW_LIBS) $(SRC)/main.c
--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <curl/curl.h>
 #include "test.h"
 
 int main() 
 {
-	puts("Yay!");
-	test();
+	CURL *curl;
+	if (curl == NULL) 
+	{
+		puts("cURL failed to load. Ooopsies!");
+	}
+	if (curl) 
+	{
+		puts("cURL armed");
+	}
 	return 0;
 }
